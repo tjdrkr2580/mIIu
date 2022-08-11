6b3dd9cc96ffc24bffc992e416ddc241d38a8540
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-12 04:09:54 +0900

    추가 Header components : barcode, p

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,34 @@
 import React from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
+import Barcode from "../../assets/barcode.png";
 
 const Header = () => {
   return (
     <header className="web-header">
-      <Link to="/">
-        <a className="logo">mIIu</a>
-      </Link>
-      <ul className="web-navs">
+      <div className="header-wrapper">
         <Link to="/">
-          <li className="web-nav"></li>
+          <a className="logo">mIIu</a>
         </Link>
-        <Link to="/">
-          <li className="web-nav">About</li>
-        </Link>
-        <Link to="/">
-          <li className="web-nav">Musics</li>
-        </Link>
-        <Link to="/">
-          <li className="web-nav">Contact</li>
-        </Link>
-      </ul>
+        <ul className="web-navs">
+          <Link to="/">
+            <li className="web-nav"></li>
+          </Link>
+          <Link to="/">
+            <li className="web-nav">About</li>
+          </Link>
+          <Link to="/">
+            <li className="web-nav">Musics</li>
+          </Link>
+          <Link to="/">
+            <li className="web-nav">Contact</li>
+          </Link>
+        </ul>
+      </div>
+      <div className="header-bottom">
+        <p>Music To You.</p>
+        <img className="barcode" src={Barcode} alt="II barcode" />
+      </div>
     </header>
   );
 };
