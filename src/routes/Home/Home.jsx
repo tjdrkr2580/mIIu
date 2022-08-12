c38dc48479742f55f0f7734b1080e4c18f954946
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-12 12:17:25 +0900

    추가 aos animations, transition-group

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./home.css";
+import AOS from "aos";
+import { useEffect } from "react";
+import "aos/dist/aos.css";
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init();
+  });
   return (
     <section className="home-wrapper">
       <ul className="home-images">
@@ -10,25 +16,55 @@ const Home = () => {
             <img src={require("../../assets/pic1.png")} alt="pic1" />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic3.png")} alt="pic3" />
+            <img
+              src={require("../../assets/pic3.png")}
+              alt="pic3"
+              data-aos="flip-left"
+              data-aos-offset="400"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic5.png")} alt="pic5" />
+            <img
+              src={require("../../assets/pic5.png")}
+              alt="pic5"
+              data-aos="flip-left"
+              data-aos-offset="500"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic7.png")} alt="pic7" />
+            <img
+              src={require("../../assets/pic7.png")}
+              alt="pic7"
+              data-aos="flip-left"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic9.png")} alt="pic9" />
+            <img
+              src={require("../../assets/pic9.png")}
+              alt="pic9"
+              data-aos="flip-left"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic11.png")} alt="pic11" />
+            <img
+              src={require("../../assets/pic11.png")}
+              alt="pic11"
+              data-aos="flip-left"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic13.png")} alt="pic13" />
+            <img
+              src={require("../../assets/pic13.png")}
+              alt="pic13"
+              data-aos="flip-left"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic15.png")} alt="pic15" />
+            <img
+              src={require("../../assets/pic15.png")}
+              alt="pic15"
+              data-aos="flip-left"
+            />
           </li>
         </div>
         <div className="g2">
@@ -36,22 +72,47 @@ const Home = () => {
             <img src={require("../../assets/pic2.png")} alt="pic2" />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic4.png")} alt="pic4" />
+            <img
+              src={require("../../assets/pic4.png")}
+              alt="pic4"
+              data-aos="flip-left"
+              data-aos-offset="600"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic6.png")} alt="pic6" />
+            <img
+              src={require("../../assets/pic6.png")}
+              alt="pic6"
+              data-aos="flip-left"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic8.png")} alt="pic8" />
+            <img
+              src={require("../../assets/pic8.png")}
+              alt="pic8"
+              data-aos="flip-left"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic10.png")} alt="pic10" />
+            <img
+              src={require("../../assets/pic10.png")}
+              alt="pic10"
+              data-aos="flip-left"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic12.png")} alt="pic12" />
+            <img
+              src={require("../../assets/pic12.png")}
+              alt="pic12"
+              data-aos="flip-left"
+            />
           </li>
           <li className="home-image">
-            <img src={require("../../assets/pic14.png")} alt="pic14" />
+            <img
+              src={require("../../assets/pic14.png")}
+              alt="pic14"
+              data-aos="flip-left"
+            />
           </li>
         </div>
       </ul>
